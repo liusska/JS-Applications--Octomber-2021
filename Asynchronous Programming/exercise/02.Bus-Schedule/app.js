7e51f419b31341be2b6aa7dfc29870f2b6301db5
unknown <lyudmila_ds@abv.bg>
2021-11-03 20:40:33 +0200

    Added 2.Bus Schedule

--- Asynchronous Programming/exercise/02.Bus-Schedule/app.js
+++ Asynchronous Programming/exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,39 @@
 function solve() {
 
     function depart() {
-        console.log('Depart TODO...');
+        let nextStopId = 'depot';
+        let stopInfoSpan = document.querySelector('#info .info');
+        let departBtn = document.getElementById('depart');
+        let arriveBtn = document.getElementById('arrive');
+
+        if(stopInfoSpan.getAttribute('data-next-stop-id') !== null){
+            nextStopId = stopInfoSpan.getAttribute('data-next-stop-id')
+        }
+
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`)
+            .then(body => body.json())
+            .then(stopInfo => {
+                stopInfoSpan.setAttribute('data-stop-name', stopInfo.name);
+                stopInfoSpan.setAttribute('data-next-stop-id', stopInfo.next);
+                stopInfoSpan.textContent = `Next stop ${stopInfo.name}`;
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            })
+            .catch(err => {
+                stopInfoSpan.textContent = 'Error';
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            });
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        let stopInfospan = document.querySelector('#info .info');
+        let departBtn = document.getElementById('depart');
+        let arriveBtn = document.getElementById('arrive');
+        let stopName = stopInfospan.getAttribute('data-stop-name');
+        stopInfospan.textContent = `Arriving at ${stopName}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
