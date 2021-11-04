933093717fe3fe9da6eeb32df289cbb5633ab1b3
unknown <lyudmila_ds@abv.bg>
2021-11-04 20:54:46 +0200

    Added Asynchronous Programming - exercise

--- Asynchronous Programming/exercise/03.Forecaster/app.js
+++ Asynchronous Programming/exercise/03.Forecaster/app.js
@@ -1,5 +1,127 @@
 function attachEvents() {
-    console.log("TODO...");
+    let getWeatherBtn = document.getElementById('submit');
+    getWeatherBtn.addEventListener('click', getWeatherHandler);
+    let locationInput = document.getElementById('location');
+
+    let conditions = {
+        'Sunny': () => '☀',
+        'Partly sunny': () => '⛅',
+        'Overcast': () => '☁',
+        'Rain': () => '☂'
+    }
+
+    function getWeatherHandler() {
+        let forecastContainer = document.getElementById('forecast');
+        forecastContainer.style.display = 'block';
+        let currentForecastContainer = document.querySelector('#current');
+        Array.from(currentForecastContainer.querySelectorAll('div')).forEach((el, i) => {
+            i !== 0 ? el.remove() : el;
+        })
+        let upcomingForecastContainer = document.querySelector('#upcoming');
+        Array.from(upcomingForecastContainer.querySelectorAll('div')).forEach((el, i) => {
+            i !== 0 ? el.remove() : el;
+        })
+
+        fetch('http://localhost:3030/jsonstore/forecaster/locations')
+            .then(body => body.json())
+            .then(locations => {
+                let locationName = locationInput.value;
+                let location = locations.find(el => el.name === locationName);
+                return fetch(`http://localhost:3030/jsonstore/forecaster/today/${location.code}`)
+                    .then(body => body.json())
+                    .then(currentWeatherReport => ({code: location.code, currentWeatherReport}));
+            })
+            .then(({code, currentWeatherReport}) => {
+                let htmlReport = createCurrentWeatherElement(currentWeatherReport);
+
+                currentForecastContainer.appendChild(htmlReport);
+
+                return fetch(`http://localhost:3030/jsonstore/forecaster/upcoming/${code}`)
+            })
+            .then(x => x.json())
+            .then(upcommingWeatherReport => {
+                console.log(upcommingWeatherReport);
+                let upcomingForecast = createUpcommingWeatherElement(upcommingWeatherReport);
+                upcomingForecastContainer.appendChild(upcomingForecast);
+            })
+            .catch(err => {
+                let errorDiv = document.createElement('div');
+                errorDiv.classList.add('label');
+                errorDiv.textContent = 'Error';
+                currentForecastContainer.appendChild(errorDiv);
+            });
+
+        function createUpcommingWeatherElement(weatherReport) {
+            let forecastInfoDiv = document.createElement('div');
+            forecastInfoDiv.classList.add('forecast-info');
+
+            let day1html = createDayReport(weatherReport.forecast[0]);
+            let day2html = createDayReport(weatherReport.forecast[1]);
+            let day3html = createDayReport(weatherReport.forecast[2]);
+
+            forecastInfoDiv.appendChild(day1html);
+            forecastInfoDiv.appendChild(day2html);
+            forecastInfoDiv.appendChild(day3html);
+            return forecastInfoDiv;
+
+        }
+
+        function createDayReport(forecast) {
+            let upcomingSpan = document.createElement('span');
+            upcomingSpan.classList.add('upcoming');
+
+            let symbolSpan = document.createElement('span');
+            symbolSpan.classList.add('symbol');
+            symbolSpan.textContent = conditions[forecast.condition]();
+
+            let temperatureSpan = document.createElement('span');
+            temperatureSpan.classList.add('forecast-data')
+            temperatureSpan.textContent = `${forecast.low}°/${forecast.high}°`;
+
+            let weatherSpan = document.createElement('span');
+            weatherSpan.classList.add('forecast-data')
+            weatherSpan.textContent = forecast.condition;
+
+            upcomingSpan.appendChild(symbolSpan);
+            upcomingSpan.appendChild(temperatureSpan);
+            upcomingSpan.appendChild(weatherSpan);
+
+            return upcomingSpan;
+
+        }
+
+        function createCurrentWeatherElement(weatherReport) {
+            let forecastDiv = document.createElement('div');
+            forecastDiv.classList.add('forecasts');
+
+            let conditionSymbolSpan = document.createElement('span');
+            conditionSymbolSpan.classList.add('condition', 'symbol');
+            conditionSymbolSpan.textContent = conditions[weatherReport.forecast.condition]();
+
+            let conditionSpan = document.createElement('span');
+            conditionSpan.classList.add('condition');
+
+            let nameSpan = document.createElement('span');
+            nameSpan.classList.add('forecast-data')
+            nameSpan.textContent = weatherReport.name;
+
+            let temperatureSpan = document.createElement('span');
+            temperatureSpan.classList.add('forecast-data')
+            temperatureSpan.textContent = `${weatherReport.forecast.low}°/${weatherReport.forecast.high}°`;
+
+            let weatherSpan = document.createElement('span');
+            weatherSpan.classList.add('forecast-data')
+            weatherSpan.textContent = weatherReport.forecast.condition;
+
+            conditionSpan.appendChild(nameSpan);
+            conditionSpan.appendChild(temperatureSpan);
+            conditionSpan.appendChild(weatherSpan);
+
+            forecastDiv.appendChild(conditionSymbolSpan);
+            forecastDiv.appendChild(conditionSpan);
+            return forecastDiv;
+        }
+    }
 }
 
 attachEvents();
--- Asynchronous Programming/exercise/05. Accordion/accordion.js
+++ Asynchronous Programming/exercise/05. Accordion/accordion.js
@@ -1,3 +1,66 @@
-function solution() {
-    //TODO .....
+window.addEventListener('DOMContentLoaded', start);
+
+
+async function start() {
+    const main = document.getElementById('main');
+    const articles = await getArticles();
+    // main.replaceChildren();
+    articles.map(createPreview).forEach(el => main.appendChild(el));
+
+
+}
+
+function createPreview(article) {
+    const element = document.createElement('div');
+    element.className = 'accordion';
+    element.innerHTML = `<div class="head">
+                <span>${article.title}</span>
+                <button class="button" id="${article._id}">More</button>
+            </div>`;
+
+    element.querySelector('button').addEventListener('click', () => {
+        togglePreview(article._id, element);
+    })
+
+    return element;
+}
+
+async function togglePreview(id, preview) {
+    const article = await getArticleById(id);
+
+    const divElement = document.createElement('div');
+    divElement.classList.add('extra')
+    preview.appendChild(divElement)
+
+    const p = document.createElement('p');
+    p.textContent = article.content;
+
+    const button = preview.querySelector('.button')
+
+    if(button.textContent === 'More'){
+        preview.appendChild(p);
+        button.textContent = 'Less';
+    }else{
+        preview.querySelector('p').remove();
+        button.textContent = 'More'
+
+    }
+}
+
+
+async function getArticles() {
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return Object.values(data);
+}
+
+async function getArticleById(id) {
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/details/' + id;
+
+    const response = await fetch(url);
+    return  await response.json();
+
 }
