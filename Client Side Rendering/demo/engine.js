611fcf83755ad0beb219232fa2eff21e377728cf
unknown <lyudmila_ds@abv.bg>
2021-11-18 13:12:12 +0200

    Added demo in Client Side Rendering

---
+++ Client Side Rendering/demo/engine.js
@@ -0,0 +1,11 @@
+export function renderTemplate(templateAsString){
+    const pattern = /{{(.+?)}}/gm;
+
+    return (data) => {
+        return templateAsString.replace(pattern, (match, propName) => {
+            return data[propName];
+        }); 
+    }
+
+
+}
