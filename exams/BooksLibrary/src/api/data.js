1a3e997442de74e9ced8e38c0bc086eab28c31c6
unknown <lyudmila_ds@abv.bg>
2021-11-28 15:13:38 +0200

    Added bonus functionality - Likes

--- exams/BooksLibrary/src/api/data.js
+++ exams/BooksLibrary/src/api/data.js
@@ -9,7 +9,7 @@ export async function getAllBooks(){
     return api.get('/data/books?sortBy=_createdOn%20desc');
 }
 
-// // for details, edit page
+// for details, edit page
 export async function getBookById(id){
     return api.get('/data/books/' + id);
 }
@@ -24,11 +24,35 @@ export async function createBook(book){
     return api.post('/data/books', book);
 }
 
-// // for delete item in details page
+// for delete item in details page
 export async function deleteById(id){
     return api.del('/data/books/' + id);
 }
 
+// for edit page
 export async function editBook(id, meme){
     return api.put('/data/books/' + id, meme);
 }
+
+// bonus functionality
+
+// request to add a like
+export async function likeBook(bookId){
+    return api.post('/data/likes', {
+         bookId 
+    });
+}
+
+// request to get total likes count for a book
+export async function getLikesByBookId(bookId){
+    return api.get(`/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`);
+}
+
+// request to get like for a book from specific user
+export async function getMyLikesByBookId(bookId, userId){
+    return api.get(`/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`);
+}
+
+// window.likeBook = likeBook;
+// window.getLikesByBookId = getLikesByBookId;
+// window.getMyLikesByBookId = getMyLikesByBookId;
